9622d55b4d650d826a319a86d780f4b64cd17c1f
francogabrieloliveros <francogabrieloliveros@gmail.com>
2026-08-01 17:41:37 +0800

    Stage 1: connect via .env and create table

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -1,44 +1,45 @@
 import type Task from "../models/task.model.ts";
-import db from "../config/db.ts";
+import client from "../db/db.ts";
 
 class TaskRepository {
   findAll = async (): Promise<Task[]> => {
-    return db.prepare(`SELECT * FROM tasks`).all() as Task[];
+    const { rows } = await client.query(`SELECT * FROM tasks`);
+    return rows as Task[];
   };
 
-  findById = async (id: number): Promise<Task | undefined> => {
-    return db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(id) as
-      Task | undefined;
-  };
+  // findById = async (id: number): Promise<Task | undefined> => {
+  //   return db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(id) as
+  //     Task | undefined;
+  // };
 
-  create = async (data: Omit<Task, "id">): Promise<Task> => {
-    const insert = db.prepare(`INSERT INTO tasks (title, done) VALUES (?, ?)`);
-    const info = insert.run(data.title, data.done ? 1 : 0);
+  // create = async (data: Omit<Task, "id">): Promise<Task> => {
+  //   const insert = db.prepare(`INSERT INTO tasks (title, done) VALUES (?, ?)`);
+  //   const info = insert.run(data.title, data.done ? 1 : 0);
 
-    return { id: info.lastInsertRowid as number, ...data };
-  };
+  //   return { id: info.lastInsertRowid as number, ...data };
+  // };
 
-  update = async (id: number, data: Partial<Task>): Promise<Task | null> => {
-    const task = await this.findById(id);
-    if (!task) return null;
+  // update = async (id: number, data: Partial<Task>): Promise<Task | null> => {
+  //   const task = await this.findById(id);
+  //   if (!task) return null;
 
-    const title = data.title ?? task.title;
-    const done = data.done ?? task.done;
+  //   const title = data.title ?? task.title;
+  //   const done = data.done ?? task.done;
 
-    db.prepare(
-      `
-      UPDATE tasks
-      SET title = ?, done = ?
-      WHERE id = ?`,
-    ).run(title, done ? 1 : 0, id);
+  //   db.prepare(
+  //     `
+  //     UPDATE tasks
+  //     SET title = ?, done = ?
+  //     WHERE id = ?`,
+  //   ).run(title, done ? 1 : 0, id);
 
-    return { ...task, title, done };
-  };
+  //   return { ...task, title, done };
+  // };
 
-  remove = async (id: number): Promise<boolean> => {
-    const info = db.prepare(`DELETE FROM tasks WHERE id = ?`).run(id);
-    return info.changes > 0;
-  };
+  // remove = async (id: number): Promise<boolean> => {
+  //   const info = db.prepare(`DELETE FROM tasks WHERE id = ?`).run(id);
+  //   return info.changes > 0;
+  // };
 }
 
 export default new TaskRepository();
