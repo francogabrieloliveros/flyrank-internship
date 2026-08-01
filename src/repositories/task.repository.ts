df7f1d8d7462a14c370c4de02db8d3d712d0a155
francogabrieloliveros <francogabrieloliveros@gmail.com>
2026-08-01 18:09:32 +0800

    Stage 3: full CRUD on Postgres

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -3,47 +3,53 @@ import client from "../db/db.ts";
 
 class TaskRepository {
   findAll = async (): Promise<Task[]> => {
-    const { rows } = await client.query(`SELECT * FROM tasks`);
-    return rows as Task[];
+    const { rows } = await client.query<Task>(`SELECT * FROM tasks`);
+    return rows;
   };
 
   findById = async (id: number): Promise<Task | undefined> => {
     const query = { text: "SELECT * FROM tasks WHERE id = $1", values: [id] };
     const {
       rows: [task],
-    } = await client.query(query);
+    } = await client.query<Task>(query);
 
-    return task as Task | undefined;
+    return task;
   };
 
-  // create = async (data: Omit<Task, "id">): Promise<Task> => {
-  //   const insert = db.prepare(`INSERT INTO tasks (title, done) VALUES (?, ?)`);
-  //   const info = insert.run(data.title, data.done ? 1 : 0);
+  create = async (data: Omit<Task, "id">): Promise<Task> => {
+    const query = {
+      text: "INSERT INTO tasks (title, done) VALUES ($1, $2) RETURNING *",
+      values: [data.title, data.done],
+    };
+    const {
+      rows: [newTask],
+    } = await client.query<Task>(query);
 
-  //   return { id: info.lastInsertRowid as number, ...data };
-  // };
+    return newTask;
+  };
 
-  // update = async (id: number, data: Partial<Task>): Promise<Task | null> => {
-  //   const task = await this.findById(id);
-  //   if (!task) return null;
+  update = async (id: number, data: Partial<Task>): Promise<Task | null> => {
+    const task = await this.findById(id);
+    if (!task) return null;
 
-  //   const title = data.title ?? task.title;
-  //   const done = data.done ?? task.done;
+    const title = data.title ?? task.title;
+    const done = data.done ?? task.done;
 
-  //   db.prepare(
-  //     `
-  //     UPDATE tasks
-  //     SET title = ?, done = ?
-  //     WHERE id = ?`,
-  //   ).run(title, done ? 1 : 0, id);
+    const query = {
+      text: "UPDATE tasks SET title = $1, done = $2 WHERE id = $3",
+      values: [title, done, id],
+    };
+    await client.query<Task>(query);
 
-  //   return { ...task, title, done };
-  // };
+    return { ...task, title, done };
+  };
 
-  // remove = async (id: number): Promise<boolean> => {
-  //   const info = db.prepare(`DELETE FROM tasks WHERE id = ?`).run(id);
-  //   return info.changes > 0;
-  // };
+  remove = async (id: number): Promise<boolean> => {
+    const query = { text: "DELETE FROM tasks WHERE id = $1", values: [id] };
+    const { rows } = await client.query<Task>(query);
+
+    return rows.length > 0;
+  };
 }
 
 export default new TaskRepository();
